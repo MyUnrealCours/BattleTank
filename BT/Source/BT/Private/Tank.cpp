bc0cd2b4ef21a09cc66ad3481d645594a56290bd
DESKTOP-K70QGPQ\Dell <Arbazirshad25@gmail.com>
2018-10-23 23:20:32 +0500

    Used CreateDefaultSubobject<>() to create an aiming component and delgate AimAt() functionality.

--- BT/Source/BT/Private/Tank.cpp
+++ BT/Source/BT/Private/Tank.cpp
@@ -8,13 +8,13 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	TankAimingComponent = CreateDefaultSubobject<UTankAImingComponent>(FName("Aiming Component"));
 
 }
 
 void ATank::AimAt(FVector HitLocation)
 {
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aims at %s"), *TankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
 // Called when the game starts or when spawned
--- BT/Source/BT/Public/Tank.h
+++ BT/Source/BT/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAImingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -19,6 +20,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UTankAImingComponent* TankAimingComponent = nullptr;
 
 public:	
 	// Called every frame
