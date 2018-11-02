454e14a39d935068284dff61ad2b19d94a2f6a97
DESKTOP-K70QGPQ\Dell <Arbazirshad25@gmail.com>
2018-11-02 22:43:40 +0500

    Turret Elevation.

--- BT/Source/BT/Private/TankAImingComponent.cpp
+++ BT/Source/BT/Private/TankAImingComponent.cpp
@@ -64,7 +64,7 @@ void UTankAImingComponent::MoveBarrel(FVector &AimDirection)
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
-	Barrel->Elevate(5);
+	Barrel->Elevate(DeltaRotator.Pitch);
 }
 
 
--- BT/Source/BT/Private/TankBarrel.cpp
+++ BT/Source/BT/Private/TankBarrel.cpp
@@ -1,10 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #include "TankBarrel.h"
+#include "Engine/World.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Barrel Elevated!"));
+	auto ElevationChange = RelativeSpeed * MaxDegreePerSecond * GetWorld()->GetDeltaSeconds();
+	auto RawElevation = RelativeRotation.Pitch + ElevationChange;
+	FMath::Clamp<float>(RawElevation, MinElevationDegree, MaxElevationDegree);
+	SetRelativeRotation(FRotator(RawElevation, 0.0, 0.0));
 }
 
 
