0b3961fab97e0f32d18f223e2a8ef73b06b381ff
DESKTOP-K70QGPQ\Dell <Arbazirshad25@gmail.com>
2019-03-01 13:54:34 +0500

    Avoiding Boolean Flags.

--- BT/Source/BT/Private/TankTrack.cpp
+++ BT/Source/BT/Private/TankTrack.cpp
@@ -1,11 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankTrack.h"
+#include "Engine/World.h"
 
 
 UTankTrack::UTankTrack()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrack::BeginPlay()
@@ -16,14 +17,19 @@ void UTankTrack::BeginPlay()
 
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp, Warning, TEXT("I'm hit, I'm hit!"))
+	DriveTrack();
+	ApplySidewaysForce();
+	CurrentThrottle = 0.0f;
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+
+
+void UTankTrack::ApplySidewaysForce()
 {
 	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
 
 	// Work-out the required acceleration this frame to correct
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
 	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
 
 	// Calculate and apply sideways (F = m a)
@@ -34,13 +40,17 @@ void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActor
 
 void UTankTrack::SetThrottle(float Throttle)
 {
+	CurrentThrottle = FMath::Clamp<float>( CurrentThrottle+Throttle, -1, +1 );
+}
+
+void UTankTrack::DriveTrack()
+{
 	auto Name = GetName();
 	//UE_LOG(LogTemp, Warning, TEXT("%s : Throttled at %f.") ,*Name ,Throttle);
 
-	auto ForceApplied = GetForwardVector() * Throttle * TruckMaxDrivingForce;
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TruckMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
-
 
--- BT/Source/BT/Public/TankTrack.h
+++ BT/Source/BT/Public/TankTrack.h
@@ -21,9 +21,13 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 		float TruckMaxDrivingForce = 400000;
 private:
+	float CurrentThrottle = 0.0f;
 	UTankTrack();
 	virtual void BeginPlay() override;
-	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	void ApplySidewaysForce();
+
+	void DriveTrack();
 
 	UFUNCTION()
 		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
